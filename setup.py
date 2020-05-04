56cceb3a768911375688206aee5f58bae161a964
dzcxs6c <radek.bednarik@tesena.com>
2020-05-04 14:00:09 +0200

    prep for github build and upload action

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup, find_packages
 from sys import platform
 
+with open("README.md", "r") as f:
+    long_description = f.read()
 
 try:
     if platform.startswith("linux"):
         setup(
             name="WatchUI",
+            author="Jan Egermaier, Radek Bednarik",
+            author_email="jan.egermaier@tesena.com",
+            description="RobotFramework library package for automated visual testing.",
+            long_description=long_description,
+            long_description_content_type="text/markdown",
+            url="https://github.com/Tesena-smart-testing/WatchUI",
             packages=find_packages(),
             install_requires=[
                 "opencv-python",
@@ -18,6 +26,12 @@ try:
                 "robotframework",
                 "robotframework-seleniumLibrary",
             ],
+            classifiers=[
+                "Programming Language :: Python :: 3",
+                "License :: OSI Approved :: MIT License",
+                "Operating System :: OS Independent",
+            ],
+            python_requires=">=3.6",
         )
     else:
         raise OSError(
