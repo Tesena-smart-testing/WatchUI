efbabc95e26f0d1d0c21cb3e6e0520608addbc41
procesor2017 <31566030+procesor2017@users.noreply.github.com>
2020-08-27 14:19:21 +0200

    new keywords for tesseract

--- WatchUI/teseract.py
+++ WatchUI/teseract.py
@@ -3,34 +3,35 @@ import pytesseract
 from pdf2image import convert_from_path, convert_from_bytes
 import tempfile
 
+
 # Otestovat
-def pdf_to_image(pathToFolder = '', outFolder = '/'):
+def pdf_to_image(path_to_folder='', out_folder='/', first_page=None, last_page=None):
     with tempfile.TemporaryFile():
-        convert_from_path(pathToFolder, output_folder=outFolder)
+        convert_from_path(path_to_folder, output_folder=out_folder, first_page=first_page, last_page=last_page)
     return True
 
 
 def rotate_image(path, rotate=0):
+    img = cv2.imread(path)
     if int(rotate) == 0:
-        img = cv2.imread(path)
         rotate_image = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
     elif int(rotate) == 1:
-        img = cv2.imread(path)
         rotate_image = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+    elif int(rotate) == 3:
+        rotate_image = cv2.rotate(img, cv2.ROTATE_180)
     else:
-        print('Zadal jsi špatnou rotate value zadej mezi 0 1 ')
+        print('Zadal jsi špatnou rotate value zadej mezi 0 3 ')
     cv2.imwrite('rotimg', rotate_image)
 
 
-
-def show_text_in_img(path, show = 'N'):
+def show_text_in_img(path, show='N', tesseract_cmd=r'C:\Program Files\Tesseract-OCR\tesseract.exe'):
     '''
     Show img with box around text :-)
 
     '''
     oldImg = cv2.imread(path)
 
-    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    pytesseract.pytesseract.tesseract_cmd = tesseract_cmd
 
     h, w, _ = oldImg.shape  # assumes color image
 
@@ -50,9 +51,10 @@ def show_text_in_img(path, show = 'N'):
     else:
         cv2.imshow('img', img)
         cv2.waitKey(0)
+        cv2.imwrite('textIMG', img)
 
 
-def full_image_to_string(path, oem=3, psm=3, language='eng'):
+def full_image_to_string(path, oem=3, psm=3, language='eng', tesseract_cmd=r'C:\Program Files\Tesseract-OCR\tesseract.exe'):
     '''
     PSM = Page Segmentation Mode
         - 0 = Orientation and script detection (OSD) only.
@@ -78,42 +80,41 @@ def full_image_to_string(path, oem=3, psm=3, language='eng'):
         - 3 = Default, based on what is available (Default)
     '''
     old_img = cv2.imread(path)
-    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    pytesseract.pytesseract.tesseract_cmd = tesseract_cmd
     custom_oem_psm_config = r'--oem ' + oem + ' --psm ' + psm
     print(pytesseract.image_to_string(old_img, config=custom_oem_psm_config, lang=language))
 
 
-def image_area_to_string(path, x1, y1, x2, y2, oem='3', psm='3', language='eng'):
-    '''
-    PSM = Page Segmentation Mode
-        - 0 = Orientation and script detection (OSD) only.
-        - 1 = Automatic page segmentation with OSD.
-        - 2 = Automatic page segmentation, but no OSD, or OCR. (not implemented)
-        - 3 = Fully automatic page segmentation, but no OSD. (Default)
-        - 4 = Assume a single column of text of variable sizes.
-        - 5 = Assume a single uniform block of vertically aligned text.
-        - 6 = Assume a single uniform block of text. (Default)
-        - 7 = Treat the image as a single text line.
-        - 8 = Treat the image as a single word.
-        - 9 = Treat the image as a single word in a circle.
-        - 10 = Treat the image as a single character.
-        - 11 = Sparse text. Find as much text as possible in no particular order.
-        - 12 = Sparse text with OSD.
-        - 13 = Raw line. Treat the image as a single text line,
-             bypassing hacks that are Tesseract-specific.
-
-    OEM = Engine Mode
-        - 0 = Original Tesseract only.
-        - 1 = Neural nets LSTM only.
-        - 2 = Tesseract + LSTM.
-        - 3 = Default, based on what is available (Default)
-    '''
-
+def image_area_on_text(path, *coordinates, oem='3', psm='3', language='eng',tesseract_cmd=r'C:\Program Files\Tesseract-OCR\tesseract.exe'):
+    string_list = []
     old_img = cv2.imread(path)
-    crop_img = old_img[int(y1): int(y2), int(x1): int(x2)]
-    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-    custom_oem_psm_config = r'--oem ' + oem + ' --psm ' + psm
-    print(pytesseract.image_to_string(crop_img, config=custom_oem_psm_config, lang=language))
+    len_coordinates = len(coordinates)
+    if len_coordinates % 4 == 0:
+        if len_coordinates / 4 == 1:
+            crop_img = old_img[int(coordinates[1]): int(coordinates[3]), int(coordinates[0]): int(coordinates[2])]
+            pytesseract.pytesseract.tesseract_cmd = tesseract_cmd
+            custom_oem_psm_config = r'--oem ' + oem + ' --psm ' + psm
+            text = pytesseract.image_to_string(crop_img, config=custom_oem_psm_config, lang=language)
+            return text
+        else:
+            num_coordinates = len_coordinates / 4
+            a = 0
+            i = 0
+            while i < num_coordinates:
+                x1 = coordinates[0 + a]
+                y1 = coordinates[1 + a]
+                x2 = coordinates[2 + a]
+                y2 = coordinates[3 + a]
+                crop_img = old_img[int(y1): int(y2), int(x1): int(x2)]
+                pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+                custom_oem_psm_config = r'--oem ' + oem + ' --psm ' + psm
+                text = pytesseract.image_to_string(crop_img, config=custom_oem_psm_config, lang=language)
+                string_list.append(text)
+                i += 1
+                a += 4
+        return string_list
+    else:
+        print('špatně zadané souřadnice')
 
 
 # show_text_in_img()
@@ -122,4 +123,4 @@ def image_area_to_string(path, x1, y1, x2, y2, oem='3', psm='3', language='eng')
 # full_image_to_string(r'C:\Users\honzik\PycharmProjects\WatchUI\Img\forpres.png')
 
 
-image_area_to_string(r'C:\Users\honzik\PycharmProjects\WatchUI\Img\forpres.png', 166, 133, 315, 175)
+image_area_on_text(r'C:\development\scripty\WatchUI\Img\forpres.png', 166, 133, 315, 175, 0, 0, 100, 100)
