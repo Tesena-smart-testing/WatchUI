c0e9f3d1a4b817492b03b9a096626bf7adb9d3d9
Jan Egermaier <jan.egermaier@gmail.com>
2022-02-28 14:18:12 +0100

    changes

---
+++ WatchUI/keywords/__init__.py
@@ -0,0 +1,12 @@
+"""
+__init__ import methodst to WatchUI.py
+"""
+from .Image import Image
+from .PDF import Pdf
+from .Tesseract import Tesseract
+
+__all__ = [
+    "Image",
+    "Pdf",
+    "Tesseract"
+]
