28856aae234d1793d85a160f8785030daa1eda85
Jan Egermaier <jfortik@gmail.com>
2023-03-26 15:49:09 +0200

    - Change version for pypi

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@ from setuptools import setup, find_packages
 
 # setup for development purposes
 # run with python setup.py develop
-setup(name="WatchUI", version="2.0.11", packages=find_packages())
+setup(name="WatchUI", version="2.0.12", packages=find_packages())
